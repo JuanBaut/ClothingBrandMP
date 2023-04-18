e8b49a794789104140e7bbaa76098cdc953fb411
JuanBaut <deserd@protonmail.com>
2023-04-18 14:49:36 -0500

    2nd commit carBrand example

--- marketplace/src/App.js
+++ marketplace/src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
+import "./App.css";
+import CarBrands from "./components/CarBrands/CarBrands.jsx";
+
+const carList = [
+  { brand: "Lamborghini", model: "Murcielago" },
+  { brand: "Ferrari", model: "F40" },
+  { brand: "Alfa Romeo", model: "155 GTA" },
+  { brand: "BMW", model: "E30 M3" },
+  { brand: "Lancia", model: "Delta Integrale" },
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          marketplace
-        </p>
-      </header>
+      <CarBrands list={carList}/>
     </div>
   );
 }
